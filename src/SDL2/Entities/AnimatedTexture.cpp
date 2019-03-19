23da692660e0c2b100e37c5991579f88f1d3a405
p1501626 <julia.francois@etu.univ-lyon1.fr>
2019-03-19 11:58:07 +0100

    Added Animated Texture class

--- src/SDL2/Entities/AnimatedTexture.cpp
+++ src/SDL2/Entities/AnimatedTexture.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "AnimatedTexture.h"
+
+AnimatedTexture::AnimatedTexture( const std::string& spritefile, int x, int y, int width, int height, int nombreImages, float vitesseAnimation, ANIMATION_DIRECTIONS direction, bool fullscreen )
+: Texture( spritefile, x, y, width, height, fullscreen ), _startX( x ), _startY( y ), _frameCount( nombreImages ), _animationSpeed( vitesseAnimation ), _animDirection( direction )
+{
+    _timer = Timer::GetInstance();
+
+    _animationTimer = 0.0f;
+    _animationDone = false;
+    _timePerFrame = _animationSpeed / _frameCount;
+
+    _wrap = loop;
+}
+
+AnimatedTexture::~AnimatedTexture()
+{
+    _timer = nullptr;
+}
+
+void AnimatedTexture::SetWrapMode( AnimatedTexture::WRAP_MODE wrap )
+{
+    _wrap = wrap;
+}
+
+void AnimatedTexture::Update()
+{
+    if (_animationDone)
+        return;
+
+    _animationTimer += _timer->GetDeltaTime();
+    if (_animationTimer >= _animationSpeed)
+    {
+        if (_wrap == once)
+        {
+            _animationDone = true;
+            _animationTimer = _animationSpeed - _timePerFrame;
+        }
+        else
+            _animationTimer -= _animationSpeed;
+    }
+
+    if (_animDirection == ANIMATION_DIRECTIONS::horizontal)
+        _clip.x = _startX + (int) (_animationTimer/_timePerFrame) * _width;
+    else
+        _clip.y = _startY + (int) (_animationTimer/_timePerFrame) * _height;
+}
--- src/SDL2/Entities/AnimatedTexture.h
+++ src/SDL2/Entities/AnimatedTexture.h
@@ -7,14 +7,47 @@
 
 // Custom headers
 #include "Texture.h"
-#include "../Core/Timer.h"
+#include "Timer.h"
 
 
 // TODO Julia je te laisse le faire ça :p
 //! Animated game entity that will be drawn
 class AnimatedTexture : public Texture
 {
+public:
+    enum ANIMATION_DIRECTIONS
+    {
+        horizontal = 0,
+        vertical
+    };
 
+    enum WRAP_MODE
+    {
+        once = 0,
+        loop
+    };
+
+private:
+
+    Timer* _timer;
+
+    int _startX, _startY;
+    ANIMATION_DIRECTIONS _animDirection;
+    WRAP_MODE _wrap;
+    int _frameCount;
+    float _animationSpeed;
+    float _timePerFrame;
+    bool _animationDone;
+
+    float _animationTimer;
+
+public:
+    AnimatedTexture( const std::string& spritefile, int x, int y, int width, int height, int nombreImages, float vitesseAnimation, ANIMATION_DIRECTIONS direction, bool fullscreen = false );
+    ~AnimatedTexture() override;
+
+    void SetWrapMode( WRAP_MODE wrap );
+
+    void Update() override;
 };
 
 
