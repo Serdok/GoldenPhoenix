194446568a356d70a62ff24b6d911ae2685a64d7
ggeof <geof.jo_h@yahoo.fr>
2019-04-02 12:59:00 +0000

    modif Pour traduction

--- src/SDL2/Entities/Screens/PauseScreen.cpp
+++ src/SDL2/Entities/Screens/PauseScreen.cpp
@@ -6,7 +6,7 @@ PauseScreen::PauseScreen(Translation* const trans) : _translation(trans)
 {
 	_background = Graphics::GetInstance();
 	_background->SetBackgroundColor(0, 0, 0);
-	_textLang = new Texture(_translation->GetTranslation(17),"Pasatona.ttf", 30, {255, 255, 255});
+	_textLang = new Texture(_translation->GetTranslation(17),"Roboto-Regular.ttf", 30, {255, 255, 255});
 	_textLang->SetPosition(Vector2f( Graphics::SCREEN_WIDTH*0.5f, Graphics::SCREEN_HEIGHT*0.5f));
 	_text = new Texture("PAUSE","Pasatona.ttf", 60, {255, 255, 255});
 	_text->SetPosition(Vector2f( Graphics::SCREEN_WIDTH*0.5f, Graphics::SCREEN_HEIGHT*0.1f));
