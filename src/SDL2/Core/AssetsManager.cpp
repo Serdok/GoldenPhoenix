7421663f410a8139e66e5cdc0c0671a808cef632
LAHNIN <anass.lahnin@etu.univ-lyon1.fr>
2019-04-10 23:59:39 +0200

    Added AssetsManager files

---
+++ src/SDL2/Core/AssetsManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by serdok on 10/04/19.
+//
+
+#include "AssetsManager.h"
---
+++ src/SDL2/Core/AssetsManager.h
@@ -0,0 +1,21 @@
+//
+// Created by serdok on 10/04/19.
+//
+
+#ifndef GOLDEN_PHOENIX_ASSETSMANAGER_H
+#define GOLDEN_PHOENIX_ASSETSMANAGER_H
+
+// Custom headers
+#include "Graphics.h"
+
+// C++ headers
+#include <map>
+
+
+class AssetsManager
+{
+private:
+
+};
+
+#endif //GOLDEN_PHOENIX_ASSETSMANAGER_H
