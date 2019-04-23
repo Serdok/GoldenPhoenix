930fa3a424372fc97e6fb168989d70a11b8ccf8f
ggeof <geof.jo_h@yahoo.fr>
2019-04-23 21:06:56 +0000

    Ajout de la class EndScreen

---
+++ src/SDL2/Entities/Screens/EndScreen.h
@@ -0,0 +1,65 @@
+#ifndef GOLDEN_PHOENIX_EndScreen_H
+#define GOLDEN_PHOENIX_EndScreen_H
+
+// Custom headers
+#include "Castle.h"
+
+#include "AnimatedTexture.h"
+#include "InputsManager.h"
+#include "AudioManager.h"
+
+
+class EndScreen : public GameEntity
+{
+private:
+
+    Castle* _castle;
+
+    InputsManager* _inputs;
+
+    int _temps = 0;
+
+    bool _quit = false;
+    Texture* Text;
+    int stepAnimFirst = 0;
+        
+    const std::string text1 = "Bravo vous etes venu à bout de ce labyrinthe...";
+    const std::string text2 = "Vous avez finis avec un score : ";
+    const std::string text3 = "Mais seriez vous capable de faire mieux....";
+    const std::string text4 = "Car après tout vous etes mort ";
+
+
+    static const int NBTEXTINSCREEN = 10;
+
+    Texture* _textscroll[NBTEXTINSCREEN];
+
+    static const int MAXSCROLL = 22;
+
+    std::string textScroll[MAXSCROLL];
+
+    int StepScroll = 0;
+
+    bool _animCredit = false;
+
+public:
+
+    explicit EndScreen(Castle* const castle);
+
+    ~EndScreen() final;
+
+    //! Process inputs and SDL2 events
+    void ProcessEvents( SDL_Event* event );
+
+    //! Update textures and game
+    void Update() final;
+
+    //! Render the buffer
+    void Render();
+
+    //! Return if quit
+    bool GetQuit() const;
+
+};
+
+
+#endif
